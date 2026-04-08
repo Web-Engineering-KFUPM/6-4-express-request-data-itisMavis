3e19bb09c2c8f755f906dcbe49308fe51bd01b8b
itisMavis <maiss.a.khalaf704@gmail.com>
2026-04-08 13:05:38 +0300

    todo 2

--- 6-4-express-request-data/server.js
+++ 6-4-express-request-data/server.js
@@ -129,6 +129,24 @@ import express from "express";
 
 const app = express();
 
+app.get("/echo", (req, res) => {
+  const { name, age } = req.query;
+
+  if (!name || !age) {
+    return res.status(400).json({
+      ok: false,
+      error: "name & age required",
+    });
+  }
+
+  res.json({
+    ok: true,
+    name,
+    age,
+    msg: `Hello ${name}, you are ${age}`,
+  });
+});
+
 app.listen(3000, () => {
   console.log("API running at http://localhost:3000");
 });
